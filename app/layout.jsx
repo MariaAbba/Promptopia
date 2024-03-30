ca7161b092328c2aac74dc9db1a6bbfea29bace1
Mary <gretchen7887@gmail.com>
2024-03-30 22:56:23 +0400

    Upd

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Nav from '@components/Nav'
 import '@styles/globals.css'
 
 export const metadata = {
@@ -15,6 +16,7 @@ const RootLayout = ({children}) => {
         
         </div>
         <main className="app">
+          <Nav />  
           {children}
         </main>
       </body>
