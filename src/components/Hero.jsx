68a5e64f2b6e695ce5a6e50fef02c5b108b73dca
Mary <gretchen7887@gmail.com>
2024-11-22 20:25:48 +0400

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import img from '../assets/img1.jpg'
-import arrow from '../assets/arrow.png'
+import arrow from '../assets/arrow1.png'
 import { MdOutlineParagliding } from 'react-icons/md'
 import { FaPeopleCarry } from 'react-icons/fa'
 import { FaRegCalendarAlt } from 'react-icons/fa'
 import { FaUsers } from 'react-icons/fa'
 import { IoOptionsOutline } from 'react-icons/io5'
 import { CiSearch } from 'react-icons/ci'
+import { RiPlanetLine } from 'react-icons/ri'
+import { FaPlaneSlash } from 'react-icons/fa'
+import { FaBiking } from 'react-icons/fa'
+import { FaUmbrellaBeach } from 'react-icons/fa'
+import { LiaWarehouseSolid } from 'react-icons/lia'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
+const responsives = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  module: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+}
+
+const type = [
+  { title: 'Adventure', icon: <FaPlaneSlash /> },
+  { title: 'Discovery', icon: <RiPlanetLine /> },
+  { title: 'Mountain Biking', icon: <FaBiking /> },
+  { title: 'Beach', icon: <FaUmbrellaBeach /> },
+  { title: 'Adventure', icon: <LiaWarehouseSolid /> },
+  // { title: 'Adventure', icon: <RiPlanetLine /> },
+]
 const Hero = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
   return (
     <div>
       <section
@@ -82,12 +117,64 @@ const Hero = () => {
           <img
             src={arrow}
             alt=""
-            className="lg:block hidden my-4 -ml-96 "
+            className="lg:block hidden my-4 -ml-96 filter brightness-0 invert"
             style={{ height: '100px', width: 'auto' }}
           />
           <p className="text-white font-semibold lg:text-3xl text-xl lg:py-0 py-8">
             Or browse the selected type
           </p>
+        </div>
+      </section>
+
+      <section className="bg-[#F3F8F6] bg-[url('/bg-shape-01.jpg)] ">
+        <div className="relative z-10 max-w-[1320px] px-6 mx-auto -mt-24">
+          <Carousel
+            responsive={responsives}
+            infinite
+            autoPlay={true}
+            itemClass="px-2"
+          >
+            {type.map((item, index) => (
+              <div className="group">
+                <div
+                  key={index}
+                  className={`cursor-pointer p-8 rounded-lg flex flex-col justify-center items-start gap-4 ${
+                    currentSlide % type.length === index
+                      ? 'bg-green text-white'
+                      : 'bg-white text-green group-hover:bg-green group-hover:text-white'
+                  }`}
+                >
+                  <p
+                    className={`font-bold text-lg ${
+                      currentSlide % type.length === index
+                        ? 'text-white'
+                        : ' text-green group-hover:text-white'
+                    }`}
+                  >
+                    {item.title}
+                  </p>
+                  <span
+                    className={`text-7xl ${
+                      currentSlide % type.length === index
+                        ? 'text-white'
+                        : ' text-green group-hover:text-white'
+                    } `}
+                  >
+                    {item.icon}
+                  </span>
+                  <p
+                    className={`${
+                      currentSlide % type.length === index
+                        ? 'text-white'
+                        : ' text-green group-hover:text-white'
+                    } `}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Carousel>
         </div>
       </section>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-green: '#63Ab45',
-orange: '#F7921E',
+        green: '#63Ab45',
+        orange: '#F7921E',
       },
       boxShadow: {
-        'xl': '0px 10px 60px 0px rgba(0, 0, 0, 0.15)'
-      }
+        xl: '0px 10px 60px 0px rgba(0, 0, 0, 0.15)',
+      },
     },
   },
   plugins: [],
 }
-
