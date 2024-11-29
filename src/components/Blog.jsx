d09601074d3538de4d96ae4623a1dc790d7c869d
Mary <gretchen7887@gmail.com>
2024-11-29 23:39:17 +0400

    Update on Blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>
+      <section>
+        <div>
+          <div>
+            <div>
+              <span className="bg-green rounded-md opacity-15 absolute w-full h-full z-10">
+                <h6>Recent news feed</h6>
+              </span>
+              <h3>Latest news & articles from the blog</h3>
+            </div>
+            <button>Views More</button>
+          </div>
+
+          <div></div>
+        </div>
+      </section>
+    </div>
   )
 }
 
